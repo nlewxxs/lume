5b7305ac51c5857022b04e1da14456b4b07811b3
Nik Lewis <nik.lewis21@imperial.ac.uk>
2025-05-20 13:13:37 +0100

    Add neopixels

--- controller/src/controller.cpp
+++ controller/src/controller.cpp
@@ -14,6 +14,7 @@
 #include "wlan_hal.h"
 #include <climits>
 #include <cstring>
+#include "neopixel.h"
 #include <variant>
 
 // Let Device OS manage the connection to the Particle Cloud
@@ -47,7 +48,7 @@ static int32_t flex1; // index
 static int32_t flex2; // ring
 static unsigned long lastUpdate = 0;
 }
-
+/* 
 void setup() {
 
     Serial.begin(9600);
@@ -142,4 +143,61 @@ void loop() {
         // Handle background callbacks (i.e. cloud conn)
         Particle.process();
     }
-} 
+} */ 
+
+
+// IMPORTANT: Set pixel COUNT, PIN and TYPE
+#if (PLATFORM_ID == 32)
+// MOSI pin MO
+#define PIXEL_PIN SPI
+// MOSI pin D2
+// #define PIXEL_PIN SPI1
+#else // #if (PLATFORM_ID == 32)
+#define PIXEL_PIN S0
+#endif
+#define PIXEL_COUNT 12
+#define PIXEL_TYPE WS2812B
+
+Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
+// Prototypes for local build, ok to leave in for Build IDE
+void rainbow(uint8_t wait);
+uint32_t Wheel(byte WheelPos);
+
+void setup()
+{
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop()
+{
+  rainbow(20);
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    Log.info("trying");
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
